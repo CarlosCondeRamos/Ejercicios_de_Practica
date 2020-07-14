d64591db1f2495db2b3c8c79584a0d3d889466b1
CarlosCondeRamos <van_carlosc@hotmail.com>
2020-07-14 19:37:03 +0200

    Inserción inicial en el repositorio del módulo pruebagit

---
+++ pruebagit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pruebagit
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
